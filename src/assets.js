177d9a5121e57b57ddc0cbdf1362d3f9232058a0
gabrieleslava <gabriel.exlava@gmail.com>
2026-01-23 18:21:43 -0300

    Fix sprite transparency by using magenta color keying

--- src/assets.js
+++ src/assets.js
@@ -20,13 +20,45 @@ export function loadAssets(ctx) {
     ASSETS.bg.src = 'assets/images/background.png';
 
     // Monitors
-    ASSETS.monster_slime.src = 'assets/images/monster_slime.png';
-    ASSETS.monster_eye.src = 'assets/images/monster_eye.png';
-    ASSETS.monster_skeleton.src = 'assets/images/monster_skeleton.png';
+    // Use color keying to remove magenta background
+    loadWithColorKey(ASSETS.monster_slime, 'assets/images/monster_slime.png');
+    loadWithColorKey(ASSETS.monster_eye, 'assets/images/monster_eye.png');
+    loadWithColorKey(ASSETS.monster_skeleton, 'assets/images/monster_skeleton.png');
+
     ASSETS.boss_demon.src = 'assets/images/boss_demon.png';
 
 
     ASSETS.bg.onload = () => {
         bgPattern = ctx.createPattern(ASSETS.bg, 'repeat');
+    };
+}
+
+function loadWithColorKey(imgObj, src) {
+    const tempImg = new Image();
+    tempImg.src = src;
+    tempImg.onload = () => {
+        const canvas = document.createElement('canvas');
+        canvas.width = tempImg.width;
+        canvas.height = tempImg.height;
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(tempImg, 0, 0);
+
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        const data = imageData.data;
+
+        // Remove Magenta (#FF00FF)
+        for (let i = 0; i < data.length; i += 4) {
+            const r = data[i];
+            const g = data[i + 1];
+            const b = data[i + 2];
+
+            // Tolerance around 255, 0, 255
+            if (r > 240 && g < 20 && b > 240) {
+                data[i + 3] = 0; // Alpha 0
+            }
+        }
+
+        ctx.putImageData(imageData, 0, 0);
+        imgObj.src = canvas.toDataURL();
     };
 }
